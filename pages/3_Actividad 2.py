97bfb17f68a82b13217b018d1a1f92df2a2d76fc
dannyedison <dannyedison@hotmail.com>
2024-10-02 15:12:27 -0500

    Creación Actividad 2

---
+++ pages/3_Actividad 2.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import io
+
+st.title("Actividad 2 - semana 9")
+
+
+#
+#.
+#
+#
+#Elimina la columna 'Descuento' del DataFrame.
+
+
+
+
+#leer el archivo csv
+df = pd.read_csv('pages\static\Ventas.csv')
+#Imprime todo el dataframe
+#st.dataframe(df)
+
+st.text("Selecciona las filas con índices 5 a 10.")
+# Mostrar la información en Streamlit
+st.dataframe(df.iloc[5:10]) 
+
+st.text("----------")
+
+st.text("Selecciona las columnas Producto y Precio")
+# Mostrar la información en Streamlit
+st.dataframe(df.loc[:,['Producto','Precio']])
+
+st.text("----------")
+
+st.text("Filtra las filas donde el 'Precio' es mayor que 100000.")
+# Mostrar la información en Streamlit
+st.dataframe(df[df['Precio'] > 100000])
+
+st.text("----------")
+
+st.text("Crea una nueva columna llamada Descuento con un 10% del Precio.")
+# Crear nueva columna
+df['Descuento'] = df['Precio'] * 0.9
+# Mostrar la información en Streamlit
+st.dataframe(df)
+
+st.text("----------")
+
+st.text("Elimina la columna Descuento del DataFrame.")
+# Elimina la columna Descuento
+df.drop('Descuento', axis=1, inplace=True)
+st.dataframe(df)
